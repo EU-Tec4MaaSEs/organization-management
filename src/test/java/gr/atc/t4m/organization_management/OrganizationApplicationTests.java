f1994b955ad0076123c3ab8f9fe11487dcaad32e
Christina Vaia <c.vaia@atc.gr>
2025-06-24 17:19:00 +0300

    EUTE4-227:Change configuration for swagger and other minor updates in application file

--- src/test/java/gr/atc/t4m/organization_management/OrganizationApplicationTests.java
+++ src/test/java/gr/atc/t4m/organization_management/OrganizationApplicationTests.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("test")
+
 class OrganizationApplicationTests {
 
 	@Test
